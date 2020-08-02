c176fc111b255121020bbb634025e79fdcdf1808
Bradley Ramos <bradleyramos2021@u.northwestern.edu>
2020-08-01 19:53:28 -0500

    web files

---
+++ precache-manifest.b7ce93cdd8fd197e14fc7fd3e4813f5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "703e13fb4a90ae4d8a3520751a87d30b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a328a4e78c1575d77e31",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9754a0a572e42cbbe198",
+    "url": "/static/js/2.ed24a7e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ed24a7e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a328a4e78c1575d77e31",
+    "url": "/static/js/main.e1734a42.chunk.js"
+  },
+  {
+    "revision": "c49ee2f895feb92e63f3",
+    "url": "/static/js/runtime-main.d7a2d26b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
